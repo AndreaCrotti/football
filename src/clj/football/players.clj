24d9faacd900990543973f6d7acda8d6c8c2554b
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-12-01 17:38:37 +0000

    fix all the tests

--- src/clj/football/players.clj
+++ src/clj/football/players.clj
@@ -1,9 +1,9 @@
-(ns football.teams
+(ns football.players
   (:require [yesql.core :as yes]
             [clojure.data.json :as json]
             [football.core :refer [db]]))
 
-(yes/defqueries "sql/teams.sql" {:connection db})
+(yes/defqueries "sql/players.sql" {:connection db})
 
 (defn add-player [player]
   (insert-player<! {:name (:name player)
--- src/clj/football/teams.clj
+++ src/clj/football/teams.clj
@@ -1,4 +1,4 @@
-(ns football.players
+(ns football.teams
   (:require [yesql.core :as yes]
             [clojure.data.json :as json]
             [football.core :refer [db]]))
